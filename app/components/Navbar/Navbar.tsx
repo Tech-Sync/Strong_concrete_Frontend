92293eaaf5b95253d5c3761c7faaa1888c5b38cf
selman <gunduzgece546@gmail.com>
2024-01-03 00:27:26 +0300

    redux example

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,10 +1,29 @@
+"use client"
+
+import {  counterSlice, selectCount } from '@/lib/redux'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Navbar = () => {
+  const dispatch = useDispatch()
+  const count = useSelector(selectCount)
+
+
+  
+  
+
   return (
-    <div className='flex justify-between'>
+    <div className='flex p-24 bg-gray-600 text-white justify-between'>
       <div>Navbar</div>
-      
+      <div>
+        <button className="btn p-2 text-2xl" onClick={() => dispatch(counterSlice.actions.increment())}>+</button>
+        <div className="inline-block p-2 text-2xl">
+          {count}
+          </div>
+        <button className="btn p-2 text-2xl" onClick={() => dispatch(counterSlice.actions.decrement())}>-</button>
+
+        </div>
+
     </div>
   )
 }
--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -10,19 +10,19 @@ import {
 import { reducer } from './rootReducer'
 import { middleware } from './middleware'
 
-export const reduxStore = configureStore({
+export const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(middleware)
   },
 })
-export const useDispatch = () => useReduxDispatch<ReduxDispatch>()
+export const useDispatch = () => useReduxDispatch<AppDispatch>()
 export const useSelector: TypedUseSelectorHook<ReduxState> = useReduxSelector
 
 /* Types */
-export type ReduxStore = typeof reduxStore
-export type ReduxState = ReturnType<typeof reduxStore.getState>
-export type ReduxDispatch = typeof reduxStore.dispatch
+export type ReduxStore = typeof store
+export type ReduxState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 export type ReduxThunkAction<ReturnType = void> = ThunkAction<
   ReturnType,
   ReduxState,
