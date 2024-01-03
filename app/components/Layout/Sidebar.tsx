dc70f79314f5bb676ac91368e8c7222128a04bce
selman <gunduzgece546@gmail.com>
2024-01-03 22:18:18 +0300

    Add Image component to Sidebar

--- app/components/Layout/Sidebar.tsx
+++ app/components/Layout/Sidebar.tsx
@@ -4,13 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 import AnimateHeight from 'react-animate-height';
-
+import Image from 'next/image'
 import { useState, useEffect } from 'react';
 
 import { selectThemeConfig,themeConfigSlice } from '@/lib/redux/slices/themeConfigSlice';
 import t from '@/utils/getLanguage';
 const Sidebar = () => {
-    const pathname = usePathname()
+    const pathname:string = usePathname()
     const [currentMenu, setCurrentMenu] = useState<string>('');
     const [errorSubMenu, setErrorSubMenu] = useState(false);
     const themeConfig =  useSelector(selectThemeConfig)
@@ -66,7 +66,7 @@ const Sidebar = () => {
                 <div className="h-full bg-white dark:bg-black">
                     <div className="flex items-center justify-between px-4 py-3">
                         <Link href="/" className="main-logo flex shrink-0 items-center">
-                            <img className="ml-[5px] w-8 flex-none" src="/assets/images/logo.svg" alt="logo" />
+                            <Image className="ml-[5px] w-8 flex-none" src="/assets/images/logo.svg" alt="logo" width={32} height={28} />
                             <span className="align-middle text-2xl font-semibold ltr:ml-1.5 rtl:mr-1.5 dark:text-white-light lg:inline">{t('VRISTO')}</span>
                         </Link>
 
