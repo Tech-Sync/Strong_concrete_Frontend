57e8826d10b9433bb9c9765a8e4eef25fd83b7a9
selman <gunduzgece546@gmail.com>
2024-01-30 15:18:38 +0300

    header ıcons added

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -8,6 +8,20 @@ import Image from 'next/image'
 import Dropdown from './Dropdown';
 import { selectThemeConfig, themeConfigSlice } from '@/lib/redux/slices/themeConfigSlice';
 import t from '@/utils/getLanguage';
+import NotificationCongratulationsIcon from '@/app/icons/Header/NotificationCongratulationsIcon';
+import NotificationInfo from '@/app/icons/Header/NotificationInfo';
+import NotificationSomeThingWrong from '@/app/icons/Header/NotificationSomeThingWrong';
+import NotificationWarning from '@/app/icons/Header/NotificationWarning';
+import ToggleHamburgerHorizontalIcon from '@/app/icons/Header/ToggleHamburgerHorizontalIcon';
+import CalenderIcon from '@/app/icons/Header/CalenderIcon';
+import TodoListIcon from '@/app/icons/Header/TodoListIcon';
+import ChatIcon from './Sidebar/SidebarIcons/ChatIcon';
+import SearchIcon from '@/app/icons/Header/SearchIcon';
+import SearchCloseIcon from '@/app/icons/Header/SearchCloseIcon';
+import SearchMobileIcon from '@/app/icons/Header/SearchMobileIcon';
+import ToggleThemeLightIcon from '@/app/icons/Header/ToggleThemeLightIcon';
+import ToggleThemeDarkIcon from '@/app/icons/Header/ToggleThemeDarkIcon';
+import ToggleThemeSystemIcon from '@/app/icons/Header/ToggleThemeSystemIcon';
 
 const Header = () => {
 	const pathname = usePathname()
@@ -52,28 +66,28 @@ const Header = () => {
 	const [messages, setMessages] = useState([
 		{
 			id: 1,
-			image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-success-light dark:bg-success text-success dark:text-success-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg></span>',
+			image: <NotificationCongratulationsIcon />,
 			title: 'Congratulations!',
 			message: 'Your OS has been updated.',
 			time: '1hr',
 		},
 		{
 			id: 2,
-			image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-info-light dark:bg-info text-info dark:text-info-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg></span>',
+			image: <NotificationInfo />,
 			title: 'Did you know?',
 			message: 'You can switch between artboards.',
 			time: '2hr',
 		},
 		{
 			id: 3,
-			image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-danger-light dark:bg-danger text-danger dark:text-danger-light"> <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></span>',
+			image: <NotificationSomeThingWrong />,
 			title: 'Something went wrong!',
 			message: 'Send Reposrt',
 			time: '2days',
 		},
 		{
 			id: 4,
-			image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-warning-light dark:bg-warning text-warning dark:text-warning-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">    <circle cx="12" cy="12" r="10"></circle>    <line x1="12" y1="8" x2="12" y2="12"></line>    <line x1="12" y1="16" x2="12.01" y2="16"></line></svg></span>',
+			image: <NotificationWarning />,
 			title: 'Warning',
 			message: 'Your password strength is low.',
 			time: '5days',
@@ -119,19 +133,15 @@ const Header = () => {
 				<div className="relative flex w-full items-center bg-white px-5 py-2.5 dark:bg-black">
 					<div className="horizontal-logo flex items-center justify-between ltr:mr-2 rtl:ml-2 lg:hidden">
 						<Link href="/" className="main-logo flex shrink-0 items-center">
-							<img className="inline w-8 ltr:-ml-1 rtl:-mr-1" src="/assets/images/logo.svg" alt="logo" />
-							<span className="hidden align-middle text-2xl  font-semibold  transition-all duration-300 ltr:ml-1.5 rtl:mr-1.5 dark:text-white-light md:inline">VRISTO</span>
+							<Image width={32} height={28} className="inline w-8 ltr:-ml-1 rtl:-mr-1" src="/assets/images/logo.svg" alt="logo" />
+							<span className="hidden align-middle text-2xl  font-semibold  transition-all duration-300 ltr:ml-1.5 rtl:mr-1.5 dark:text-white-light md:inline">SC</span>
 						</Link>
 						<button
 							type="button"
-							className="collapse-icon flex flex-none rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary ltr:ml-2 rtl:mr-2 dark:bg-dark/40 dark:text-[#d0d2d6] dark:hover:bg-dark/60 dark:hover:text-primary lg:hidden"
+							className="collapse-icon  flex flex-none rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary ltr:ml-2 rtl:mr-2 dark:bg-dark/40 dark:text-[#d0d2d6] dark:hover:bg-dark/60 dark:hover:text-primary lg:hidden"
 							onClick={() => dispatch(themeConfigSlice.actions.toggleSidebar())}
 						>
-							<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M20 7L4 7" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-								<path opacity="0.5" d="M20 12L4 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-								<path d="M20 17L4 17" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-							</svg>
+							<ToggleHamburgerHorizontalIcon />
 						</button>
 					</div>
 
@@ -139,54 +149,17 @@ const Header = () => {
 						<ul className="flex items-center space-x-2 rtl:space-x-reverse dark:text-[#d0d2d6]">
 							<li>
 								<Link href="/apps/calendar" className="block rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary dark:bg-dark/40 dark:hover:bg-dark/60">
-									<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path
-											d="M2 12C2 8.22876 2 6.34315 3.17157 5.17157C4.34315 4 6.22876 4 10 4H14C17.7712 4 19.6569 4 20.8284 5.17157C22 6.34315 22 8.22876 22 12V14C22 17.7712 22 19.6569 20.8284 20.8284C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.8284C2 19.6569 2 17.7712 2 14V12Z"
-											stroke="currentColor"
-											strokeWidth="1.5"
-										/>
-										<path opacity="0.5" d="M7 4V2.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										<path opacity="0.5" d="M17 4V2.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										<path opacity="0.5" d="M2 9H22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-									</svg>
+									<CalenderIcon />
 								</Link>
 							</li>
 							<li>
 								<Link href="/apps/todolist" className="block rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary dark:bg-dark/40 dark:hover:bg-dark/60">
-									<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path
-											opacity="0.5"
-											d="M22 10.5V12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2H13.5"
-											stroke="currentColor"
-											strokeWidth="1.5"
-											strokeLinecap="round"
-										/>
-										<path
-											d="M17.3009 2.80624L16.652 3.45506L10.6872 9.41993C10.2832 9.82394 10.0812 10.0259 9.90743 10.2487C9.70249 10.5114 9.52679 10.7957 9.38344 11.0965C9.26191 11.3515 9.17157 11.6225 8.99089 12.1646L8.41242 13.9L8.03811 15.0229C7.9492 15.2897 8.01862 15.5837 8.21744 15.7826C8.41626 15.9814 8.71035 16.0508 8.97709 15.9619L10.1 15.5876L11.8354 15.0091C12.3775 14.8284 12.6485 14.7381 12.9035 14.6166C13.2043 14.4732 13.4886 14.2975 13.7513 14.0926C13.9741 13.9188 14.1761 13.7168 14.5801 13.3128L20.5449 7.34795L21.1938 6.69914C22.2687 5.62415 22.2687 3.88124 21.1938 2.80624C20.1188 1.73125 18.3759 1.73125 17.3009 2.80624Z"
-											stroke="currentColor"
-											strokeWidth="1.5"
-										/>
-										<path
-											opacity="0.5"
-											d="M16.6522 3.45508C16.6522 3.45508 16.7333 4.83381 17.9499 6.05034C19.1664 7.26687 20.5451 7.34797 20.5451 7.34797M10.1002 15.5876L8.4126 13.9"
-											stroke="currentColor"
-											strokeWidth="1.5"
-										/>
-									</svg>
+									<TodoListIcon />
 								</Link>
 							</li>
 							<li>
 								<Link href="/apps/chat" className="block rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 hover:text-primary dark:bg-dark/40 dark:hover:bg-dark/60">
-									<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<circle r="3" transform="matrix(-1 0 0 1 19 5)" stroke="currentColor" strokeWidth="1.5" />
-										<path
-											opacity="0.5"
-											d="M14 2.20004C13.3538 2.06886 12.6849 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22C17.5228 22 22 17.5228 22 12C22 11.3151 21.9311 10.6462 21.8 10"
-											stroke="currentColor"
-											strokeWidth="1.5"
-											strokeLinecap="round"
-										/>
-									</svg>
+									<ChatIcon />
 								</Link>
 							</li>
 						</ul>
@@ -204,16 +177,10 @@ const Header = () => {
 										placeholder="Search..."
 									/>
 									<button type="button" className="absolute inset-0 h-9 w-9 appearance-none peer-focus:text-primary ltr:right-auto rtl:left-auto">
-										<svg className="mx-auto" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-											<circle cx="11.5" cy="11.5" r="9.5" stroke="currentColor" strokeWidth="1.5" opacity="0.5" />
-											<path d="M18.5 18.5L22 22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										</svg>
+										<SearchIcon />
 									</button>
 									<button type="button" className="absolute top-1/2 block -translate-y-1/2 hover:opacity-80 ltr:right-2 rtl:left-2 sm:hidden" onClick={() => setSearch(false)}>
-										<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-											<circle opacity="0.5" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="1.5" />
-											<path d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										</svg>
+										<SearchCloseIcon />
 									</button>
 								</div>
 							</form>
@@ -222,10 +189,7 @@ const Header = () => {
 								onClick={() => setSearch(!search)}
 								className="search_btn rounded-full bg-white-light/40 p-2 hover:bg-white-light/90 dark:bg-dark/40 dark:hover:bg-dark/60 sm:hidden"
 							>
-								<svg className="mx-auto h-4.5 w-4.5 dark:text-[#d0d2d6]" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<circle cx="11.5" cy="11.5" r="9.5" stroke="currentColor" strokeWidth="1.5" opacity="0.5" />
-									<path d="M18.5 18.5L22 22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-								</svg>
+								<SearchMobileIcon />
 							</button>
 						</div>
 						<div>
@@ -236,17 +200,7 @@ const Header = () => {
 										}`}
 									onClick={() => dispatch(themeConfigSlice.actions.toggleTheme('dark'))}
 								>
-									<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<circle cx="12" cy="12" r="5" stroke="currentColor" strokeWidth="1.5" />
-										<path d="M12 2V4" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										<path d="M12 20V22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										<path d="M4 12L2 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										<path d="M22 12L20 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										<path opacity="0.5" d="M19.7778 4.22266L17.5558 6.25424" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										<path opacity="0.5" d="M4.22217 4.22266L6.44418 6.25424" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										<path opacity="0.5" d="M6.44434 17.5557L4.22211 19.7779" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										<path opacity="0.5" d="M19.7778 19.7773L17.5558 17.5551" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-									</svg>
+									<ToggleThemeLightIcon />
 								</button>
 							) : (
 								''
@@ -258,12 +212,7 @@ const Header = () => {
 										}`}
 									onClick={() => dispatch(themeConfigSlice.actions.toggleTheme('system'))}
 								>
-									<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path
-											d="M21.0672 11.8568L20.4253 11.469L21.0672 11.8568ZM12.1432 2.93276L11.7553 2.29085V2.29085L12.1432 2.93276ZM21.25 12C21.25 17.1086 17.1086 21.25 12 21.25V22.75C17.9371 22.75 22.75 17.9371 22.75 12H21.25ZM12 21.25C6.89137 21.25 2.75 17.1086 2.75 12H1.25C1.25 17.9371 6.06294 22.75 12 22.75V21.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75V1.25C6.06294 1.25 1.25 6.06294 1.25 12H2.75ZM15.5 14.25C12.3244 14.25 9.75 11.6756 9.75 8.5H8.25C8.25 12.5041 11.4959 15.75 15.5 15.75V14.25ZM20.4253 11.469C19.4172 13.1373 17.5882 14.25 15.5 14.25V15.75C18.1349 15.75 20.4407 14.3439 21.7092 12.2447L20.4253 11.469ZM9.75 8.5C9.75 6.41182 10.8627 4.5828 12.531 3.57467L11.7553 2.29085C9.65609 3.5593 8.25 5.86509 8.25 8.5H9.75ZM12 2.75C11.9115 2.75 11.8077 2.71008 11.7324 2.63168C11.6686 2.56527 11.6538 2.50244 11.6503 2.47703C11.6461 2.44587 11.6482 2.35557 11.7553 2.29085L12.531 3.57467C13.0342 3.27065 13.196 2.71398 13.1368 2.27627C13.0754 1.82126 12.7166 1.25 12 1.25V2.75ZM21.7092 12.2447C21.6444 12.3518 21.5541 12.3539 21.523 12.3497C21.4976 12.3462 21.4347 12.3314 21.3683 12.2676C21.2899 12.1923 21.25 12.0885 21.25 12H22.75C22.75 11.2834 22.1787 10.9246 21.7237 10.8632C21.286 10.804 20.7293 10.9658 20.4253 11.469L21.7092 12.2447Z"
-											fill="currentColor"
-										/>
-									</svg>
+									<ToggleThemeDarkIcon />
 								</button>
 							)}
 							{themeConfig.theme === 'system' && (
@@ -273,22 +222,14 @@ const Header = () => {
 										}`}
 									onClick={() => dispatch(themeConfigSlice.actions.toggleTheme('light'))}
 								>
-									<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path
-											d="M3 9C3 6.17157 3 4.75736 3.87868 3.87868C4.75736 3 6.17157 3 9 3H15C17.8284 3 19.2426 3 20.1213 3.87868C21 4.75736 21 6.17157 21 9V14C21 15.8856 21 16.8284 20.4142 17.4142C19.8284 18 18.8856 18 17 18H7C5.11438 18 4.17157 18 3.58579 17.4142C3 16.8284 3 15.8856 3 14V9Z"
-											stroke="currentColor"
-											strokeWidth="1.5"
-										/>
-										<path opacity="0.5" d="M22 21H2" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-										<path opacity="0.5" d="M15 15H9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
-									</svg>
+									<ToggleThemeSystemIcon />
 								</button>
 							)}
 						</div>
 						<div className="dropdown shrink-0">
 							<Dropdown
 								offset={[0, 8]}
-								placement='bottom-start'
+								placement='bottom-end'
 								btnClassName="block p-2 rounded-full bg-white-light/40 dark:bg-dark/40 hover:text-primary hover:bg-white-light/90 dark:hover:bg-dark/60"
 								button={
 									<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -321,7 +262,7 @@ const Header = () => {
 												{messages.map((message) => {
 													return (
 														<div key={message.id} className="flex items-center py-3 px-5">
-															<div dangerouslySetInnerHTML={createMarkup(message.image)}></div>
+															<div >{message.image}</div>
 															<span className="px-3 dark:text-gray-500">
 																<div className="text-sm font-semibold dark:text-white-light/90">{message.title}</div>
 																<div>{message.message}</div>
@@ -385,7 +326,7 @@ const Header = () => {
 						<div className="dropdown shrink-0">
 							<Dropdown
 								offset={[0, 8]}
-								placement={`${isRtl ? 'bottom-start' : 'bottom-end'}`}
+								placement={`${isRtl ? 'bottom-end' : 'bottom-end'}`}
 								btnClassName="relative block p-2 rounded-full bg-white-light/40 dark:bg-dark/40 hover:text-primary hover:bg-white-light/90 dark:hover:bg-dark/60"
 								button={
 									<span>
@@ -420,7 +361,7 @@ const Header = () => {
 														<div className="group flex items-center px-4 py-2">
 															<div className="grid place-content-center rounded">
 																<div className="relative h-12 w-12">
-																	<img className="h-12 w-12 rounded-full object-cover" alt="profile" src={`/assets/images/${notification.profile}`} />
+																	<Image width={48} height={48} className="h-12 w-12 rounded-full object-cover" alt="profile" src={`/assets/images/${notification.profile}`} />
 																	<span className="absolute right-[6px] bottom-0 block h-2 w-2 rounded-full bg-success"></span>
 																</div>
 															</div>
@@ -484,14 +425,14 @@ const Header = () => {
 						<div className="dropdown flex shrink-0">
 							<Dropdown
 								offset={[0, 8]}
-								placement='bottom-start'
+								placement='bottom-end'
 								btnClassName="relative group block"
-								button={<img className="h-9 w-9 rounded-full object-cover saturate-50 group-hover:saturate-100" src="/assets/images/user-profile.jpeg" alt="userProfile" />}
+								button={<Image width={36} height={36} className="h-9 w-9 rounded-full object-cover saturate-50 group-hover:saturate-100" src="/assets/images/user-profile.jpeg" alt="userProfile" />}
 							>
 								<ul className="w-[230px] !py-0 font-semibold text-dark dark:text-white-dark dark:text-white-light/90">
 									<li>
 										<div className="flex items-center px-4 py-4">
-											<img className="h-10 w-10 rounded-md object-cover" src="/assets/images/user-profile.jpeg" alt="userProfile" />
+											<Image width={36} height={36} className="h-10 w-10 rounded-md object-cover" src="/assets/images/user-profile.jpeg" alt="userProfile" />
 											<div className="ltr:pl-4 rtl:pr-4">
 												<h4 className="text-base">
 													John Doe
