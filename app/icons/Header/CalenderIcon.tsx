4524877df82de7737bf9807a379471b7a15321f0
selman <gunduzgece546@gmail.com>
2024-01-30 17:16:54 +0300

    Update import paths for header icons

--- app/icons/Header/CalenderIcon.tsx
+++ app/icons/Header/CalenderIcon.tsx
@@ -1,5 +1,5 @@
 
-const CalenderIcon = () => {
+export const CalenderIcon = () => {
   return (
    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
    <path
@@ -13,5 +13,3 @@ const CalenderIcon = () => {
   </svg>
   )
 }
-
-export default CalenderIcon
