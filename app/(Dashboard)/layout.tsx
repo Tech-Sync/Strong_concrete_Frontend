8c6bdfaf18e4b3ae02cc64d394ded8760e0a0ae7
Kadir-Gursoy <mnkmcc9@gmail.com>
2024-02-07 11:55:07 +0300

    purchases

--- app/(Dashboard)/layout.tsx
+++ app/(Dashboard)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import DefaultLayout from "../components/Layout/DefaultLayout";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
-import {MantineProvider} from "@mantine/core"
+
 
 export default async function Layout({
   children,
@@ -14,11 +14,10 @@ export default async function Layout({
   return (
     <SessionProvider session={session} refetchInterval={60 * 60 * 24}>
 
-      <MantineProvider>
  
       <DefaultLayout>{children}</DefaultLayout>
 
-      </MantineProvider>
+    
     </SessionProvider>
   );
 }
