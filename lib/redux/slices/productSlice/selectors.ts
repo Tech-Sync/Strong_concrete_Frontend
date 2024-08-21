f9482de81565610811c705cf5ba50e9d01352271
AliDurul <alidrl26@gmail.com>
2024-08-21 11:40:59 +0200

    Refactor redux files and remove unused code

--- lib/redux/slices/productSlice/selectors.ts
+++
@@ -1,5 +0,0 @@
-import type { RootState } from '@/lib/redux'
-
-export const selectProducts = (state: RootState) => state.product.products
-export const selectProductStatus = (state: RootState) => state.product.status
-
--- lib/redux/slices/productionSlice/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./productionSlice";
-export * from "./selectors";
-export * from "./thunks";
-
