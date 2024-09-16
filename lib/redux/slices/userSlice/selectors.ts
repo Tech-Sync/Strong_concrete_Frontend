45ab11dcb6254757e29f1eb2fbdc8206995d750b
AliDurul <alidrl26@gmail.com>
2024-09-16 11:44:00 +0200

    Remove unused code related to saleSlice and themeConfigSlice

--- lib/redux/slices/userSlice/selectors.ts
+++
@@ -1,5 +0,0 @@
-import type { RootState } from '@/lib/redux'
-
-export const selectUsers = (state: RootState) => state.user.users
-export const selectUserStatus = (state: RootState) => state.user.status
-
