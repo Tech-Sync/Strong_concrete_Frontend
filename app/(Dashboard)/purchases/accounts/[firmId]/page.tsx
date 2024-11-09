d4016826681a29bdfb1d6810b30f590e33efbc21
AliDurul <alidrl26@gmail.com>
2024-11-09 14:20:41 +0200

    Refactor Purchase Account and Sale components: update data access patterns to handle nested data structures for purchases and firms, improving state management and data retrieval.

--- app/(Dashboard)/purchases/accounts/[firmId]/page.tsx
+++ app/(Dashboard)/purchases/accounts/[firmId]/page.tsx
@@ -3,11 +3,11 @@ import PurchaseAccFirmInfoInfoTable from './_components/PurchaseAccFirmInfoTable
 import BreadCrumb from '@/app/components/common/BreadCrumb'
 import { getAllPurchases } from '@/lib/features/purchase/purchaseActions'
 
-export default async function PurchaseAccFirmPage({ params }: { params: { firmId: { firmId?: string | undefined } } }) {
-    
-    
+export default async function PurchaseAccFirmPage({ params }: { params: { firmId: string } }) {
+
+
     const rowData = await getAllPurchases(params.firmId)
-    
+
 
     return (
         <>
